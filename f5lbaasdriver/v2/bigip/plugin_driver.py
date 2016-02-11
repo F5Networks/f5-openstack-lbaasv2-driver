ae74434a2134b47e49a9ae99f400cc74dbefb2f1
Jeffrey Longstaff <j.longstaff@f5.com>
2016-02-11 13:47:47 -0700

    Add initial driver implementation
    Issues:
    Fixes #11
    
    Problem: Need to start with clean code.
    
    Analysis: Removed modules which contain invalid or non-working code.
    
    Tests:

--- f5lbaasdriver/v2/bigip/plugin_driver.py
+++ f5lbaasdriver/v2/bigip/plugin_driver.py
@@ -16,11 +16,6 @@ import sys
 
 from oslo_config import cfg
 from oslo_log import log as logging
-from oslo_utils import importutils
-
-from f5lbaasdriver.v2.bigip.agent_rpc import LBaaSv2AgentRPC
-from f5lbaasdriver.v2.bigip.plugin_rpc import LBaaSv2PluginCallbacksRPC
-from f5lbaasdriver.v2.bigip.service_builder import LBaaSv2ServiceBuilder
 
 
 LOG = logging.getLogger(__name__)
@@ -54,236 +49,53 @@ class F5DriverV2(object):
         self.core_plugin = core_plugin
         self.env = env
 
-        # Service agent scheduler
-        self.scheduler = importutils.import_object(
-            cfg.CONF.f5_loadbalancer_pool_scheduler_driver_v2)
-        # Registered RPC Callback call by agents
-        self.plugin_rpc = LBaaSv2PluginCallbacksRPC(driver=self)
-        # RPC Proxy to call agent queues
-        self.agent_rpc = LBaaSv2AgentRPC(driver=self)
-        # Service builder
-        self.service_def = LBaaSv2ServiceBuilder(driver=self)
-
     def lb_create(self, context, lb):
-        agent = self.scheduler.schedule(self.plugin, context, lb, self.env)
-        service = self.service_def.build(self, lb['id'])
-        self.agent_rpc.lb_create(context, agent, lb=lb, service=service)
+        pass
 
     def lb_update(self, context, old_lb, lb):
-        agent = self.scheduler.get_lbaas_agent_hosting_loadbalancer(
-            self.plugin,
-            context,
-            lb['id'],
-            self.env
-        )
-        service = self.service_def.build(self, lb['id'])
-        self.agent_rpc.lb_update(context, agent,
-                                 old_lb=old_lb, lb=lb,
-                                 service=service)
+        pass
 
     def lb_delete(self, context, lb):
-        agent = self.scheduler.get_lbaas_agent_hosting_loadbalancer(
-            self.plugin,
-            context,
-            lb['id'],
-            self.env
-        )
-        service = self.service_def.build(self, lb['id'])
-        self.agent_rpc.lb_delete(context, agent, lb=lb, service=service)
+        pass
 
     def lb_refresh(self, context, lb):
-        agent = self.scheduler.get_lbaas_agent_hosting_loadbalancer(
-            self.plugin,
-            context,
-            lb['id'],
-            self.env
-        )
-        service = self.service_def.build(self, lb['id'])
-        self.agent_rpc.lb_refresh(context, agent, lb=lb, service=service)
+        pass
 
     def lb_stats(self, context, lb):
-        agent = self.scheduler.get_lbaas_agent_hosting_loadbalancer(
-            self.plugin,
-            context,
-            lb['id'],
-            self.env
-        )
-        self.agent_rpc.lb_stats(context, agent, lb=lb)
+        pass
 
     def listener_create(self, context, listener):
-        agent = self.scheduler.get_lbaas_agent_hosting_loadbalancer(
-            self.plugin,
-            context,
-            listener['loadbalancer_id'],
-            self.env
-        )
-        service = self.service_def.build(
-            self,
-            listener['loadbalancer_id']
-        )
-        self.agent_rpc.listener_create(context, agent,
-                                       listener=listener,
-                                       service=service)
+        pass
 
     def listener_update(self, context, old_listener, listener):
-        agent = self.scheduler.get_lbaas_agent_hosting_loadbalancer(
-            self.plugin,
-            context,
-            listener['loadbalancer_id'],
-            self.env
-        )
-        service = self.service_def.build(
-            self,
-            listener['loadbalancer_id']
-        )
-        self.agent_rpc.listener_update(context, agent,
-                                       old_listener=old_listener,
-                                       listener=listener,
-                                       service=service)
+        pass
 
     def listener_delete(self, context, listener):
-        agent = self.scheduler.get_lbaas_agent_hosting_loadbalancer(
-            self.plugin,
-            context,
-            listener['loadbalancer_id'],
-            self.env
-        )
-        service = self.service_def.build(
-            self,
-            listener['loadbalancer_id']
-        )
-        self.agent_rpc.listener_delete(context, agent,
-                                       listener=listener,
-                                       service=service)
+        pass
 
     def pool_create(self, context, pool):
-        agent = self.scheduler.get_lbaas_agent_hosting_loadbalancer(
-            self.plugin,
-            context,
-            pool['root_loadbalancer']['id'],
-            self.env
-        )
-        service = self.service_def.build(
-            self,
-            pool['root_loadbalancer']['id']
-        )
-        self.agent_rpc.pool_create(context, agent,
-                                   pool=pool, service=service)
+        pass
 
     def pool_update(self, context, old_pool, pool):
-        agent = self.scheduler.get_lbaas_agent_hosting_loadbalancer(
-            self.plugin,
-            context,
-            pool['root_loadbalancer']['id'],
-            self.env
-        )
-        service = self.service_def.build(
-            self,
-            pool['root_loadbalancer']['id']
-        )
-        self.agent_rpc.pool_update(context, agent,
-                                   old_pool=old_pool,
-                                   pool=pool,
-                                   service=service)
+        pass
 
     def pool_delete(self, context, pool):
-        agent = self.scheduler.get_lbaas_agent_hosting_loadbalancer(
-            self.plugin,
-            context,
-            pool['root_loadbalancer']['id'],
-            self.env
-        )
-        service = self.service_def.build(
-            self,
-            pool['root_loadbalancer']['id']
-        )
-        self.agent_rpc.pool_delete(context, agent,
-                                   pool=pool,
-                                   service=service)
+        pass
 
     def member_create(self, context, member):
-        agent = self.scheduler.get_lbaas_agent_hosting_loadbalancer(
-            self.plugin,
-            context,
-            member['root_loadbalancer']['id'],
-            self.env
-        )
-        service = self.service_def.build(
-            self,
-            member['root_loadbalancer']['id']
-        )
-        self.agent_rpc.member_create(context, agent,
-                                     member=member,
-                                     service=service)
+        pass
 
     def member_update(self, context, old_member, member):
-        agent = self.scheduler.get_lbaas_agent_hosting_loadbalancer(
-            self.plugin,
-            context,
-            member['root_loadbalancer']['id'],
-            self.env
-        )
-        service = self.service_def.build(
-            self,
-            member['root_loadbalancer']['id']
-        )
-        self.agent_rpc.member_update(context, agent,
-                                     old_member=old_member,
-                                     member=member,
-                                     service=service)
+        pass
 
     def member_delete(self, context, member):
-        agent = self.scheduler.get_lbaas_agent_hosting_loadbalancer(
-            self.plugin,
-            context,
-            member['root_loadbalancer']['id'],
-            self.env
-        )
-        service = self.service_def.build(
-            self,
-            member['root_loadbalancer']['id']
-        )
-        self.agent_rpc.member_delete(context, agent, member, service=service)
+        pass
 
     def healthmonitor_create(self, context, hm):
-        agent = self.scheduler.get_lbaas_agent_hosting_loadbalancer(
-            self.plugin,
-            context,
-            hm['root_loadbalancer']['id'],
-            self.env
-        )
-        service = self.service_def.build(
-            self,
-            hm['root_loadbalancer']['id']
-        )
-        self.agent_rpc.healthmonitor_create(context, agent,
-                                            healthmonitor=hm, service=service)
+        pass
 
     def healthmonitor_update(self, context, old_hm, hm):
-        agent = self.scheduler.get_lbaas_agent_hosting_loadbalancer(
-            self.plugin,
-            context,
-            hm['root_loadbalancer']['id'],
-            self.env
-        )
-        service = self.service_def.build(
-            self,
-            hm['root_loadbalancer']['id']
-        )
-        self.agent_rpc.healthmonitor_update(context, agent,
-                                            old_healthmonitor=old_hm,
-                                            healthmonitor=hm, service=service)
+        pass
 
     def healthmonitor_delete(self, context, hm):
-        agent = self.scheduler.get_lbaas_agent_hosting_loadbalancer(
-            self.plugin,
-            context,
-            hm['root_loadbalancer']['id'],
-            self.env
-        )
-        service = self.service_def.build(
-            self,
-            hm['root_loadbalancer']['id']
-        )
-        self.agent_rpc.healthmonitor_delete(context, agent,
-                                            healthmonitor=hm, service=service)
+        pass
