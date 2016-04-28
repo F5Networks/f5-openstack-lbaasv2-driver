90a336520b50a42bda2f1212c6c6e1ee5ef8d66d
Philip Skergan <skergan@bld-ml-skergan.olympus.f5net.com>
2016-04-28 12:41:41 -0600

    Change git repo pull to git+ssh. Put all use of symbols in one spot in test_solution.py. Change desc of auth_url.

--- test/functional/test_solution.py
+++ test/functional/test_solution.py
@@ -37,35 +37,29 @@ def log_test_call(func):
 
 class ExecTestEnv(object):
 
-    def __init__(self, symbols):
-        self.symbols = symbols.copy()
-        if 'lbaas_version' in self.symbols:
-            lbaas_version = self.symbols['lbaas_version']
-        else:
-            lbaas_version = symbols_data.lbaas_version
-        new_symbols = {
-            'debug':            True,
-            'lbaas_version':    lbaas_version,
-            # leave the following settings alone
-            'bigip_username':   symbols_data.bigip_username,
-            'bigip_password':   symbols_data.bigip_password,
-            # The provider string for v2 needs to change to match the
-            # environment prefix in the f5 agent ini file
-            'provider':         (symbols_data.provider if lbaas_version == 2 else
-                                 'f5'),
-            'admin_name':       symbols_data.admin_name,
-            'admin_username':   symbols_data.admin_username,
-            'admin_password':   symbols_data.admin_password,
-            'tenant_name':      symbols_data.tenant_name,
-            'tenant_username':  symbols_data.tenant_username,
-            'tenant_password':  symbols_data.tenant_password,
-            'client_subnet':    symbols_data.tenant_name + '-client-v4-subnet',
-            'guest_username':   symbols_data.guest_username,
-            'guest_password':   symbols_data.guest_password,
-            'server_http_port': symbols_data.server_http_port,
-            'server_client_ip': symbols_data.server_client_ip
-        }
-        self.symbols.update(new_symbols)
+    def __init__(self):
+        self.symbols = {}
+        self.symbols['bigip_public_mgmt_ip']  = symbols_data.bigip_ip
+        self.symbols['client_public_mgmt_ip'] = symbols_data.client_ip
+        self.symbols['server_public_mgmt_ip'] = symbols_data.server_ip
+        self.symbols['openstack_auth_url']    = symbols_data.auth_url
+        self.symbols['lbaas_version']         = symbols_data.lbaas_version
+        self.symbols['debug']                 = True
+        self.symbols['bigip_username']        = symbols_data.bigip_username
+        self.symbols['bigip_password']        = symbols_data.bigip_password
+        self.symbols['provider']              = ('f5' if symbols_data.lbaas_version < 2 \
+                                                  else symbols_data.provider)
+        self.symbols['admin_name']            = symbols_data.admin_name
+        self.symbols['admin_username']        = symbols_data.admin_username
+        self.symbols['admin_password']        = symbols_data.admin_password
+        self.symbols['tenant_name']           = symbols_data.tenant_name
+        self.symbols['tenant_username']       = symbols_data.tenant_username
+        self.symbols['tenant_password']       = symbols_data.tenant_password
+        self.symbols['client_subnet']         = symbols_data.tenant_name + '-client-v4-subnet'
+        self.symbols['guest_username']        = symbols_data.guest_username
+        self.symbols['guest_password']        = symbols_data.guest_password
+        self.symbols['server_http_port']      = symbols_data.server_http_port
+        self.symbols['server_client_ip']      = symbols_data.server_client_ip
 
 
 def nclientmanager(symbols):
@@ -375,15 +369,7 @@ class LBaaSv2(object):
 @pytest.fixture
 def tst_setup(request, symbols):
     print 'test setup'
-    # change the following settings to use your TLC session
-    # temp solution until we integrate with Kevin's py-symbols module.
-    symbols_env = {
-        'bigip_public_mgmt_ip':     symbols.bigip_ip,
-        'client_public_mgmt_ip':    symbols.client_ip,
-        'server_public_mgmt_ip':    symbols.server_ip,
-        'openstack_auth_url':       symbols.auth_url
-    }
-    testenv = ExecTestEnv(symbols_env)
+    testenv = ExecTestEnv()
 
     def tst_teardown():
         print 'test teardown'
