8021782b5e3752f1f307e35a8960678dce1ae2e5
Jodie Putrino <j.putrino@f5.com>
2016-06-21 09:49:05 -0600

    changed intersphinx ref for heat to 'latest'

--- docs/conf.py
+++ docs/conf.py
@@ -311,7 +311,7 @@ texinfo_documents = [
 # Example configuration for intersphinx: refer to the Python standard library.
 
 intersphinx_mapping = {'heat': (
-     'http://f5-openstack-heat.readthedocs.io/en/liberty', None),
+     'http://f5-openstack-heat.readthedocs.io/en/latest', None),
      'heatplugins': (
      'http://f5-openstack-heat-plugins.readthedocs.io/en/liberty', None),
      'lbaasv1': (
