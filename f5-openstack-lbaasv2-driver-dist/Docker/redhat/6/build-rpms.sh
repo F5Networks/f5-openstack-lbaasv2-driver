22f503ea6fabfecfb78b2b3624fb0032b9dc5568
richbrowne <r.browne@f5.com>
2016-07-21 11:31:18 -0600

    Adding package files

---
+++ f5-openstack-lbaasv2-driver-dist/Docker/redhat/6/build-rpms.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -ex
+
+SRC_DIR=$1
+DIST_DIR=$2
+OS_VERSION=6
+
+DEST_DIR="${SRC_DIR}/${DIST_DIR}"
+
+echo "Building RPM packages...${DIST_DIR}"
+buildroot=$(mktemp -d /tmp/f5-openstack-lbaasv2-driver.XXXX)
+
+cp -R $SRC_DIR/* ${buildroot}
+
+pushd ${buildroot}
+
+python setup.py bdist_rpm
+
+rm -rf ${DEST_DIR}/rpms
+mkdir -p ${DEST_DIR}/rpms
+
+for pkg in $(ls dist/*.rpm); do
+  if [[ $pkg =~ ".noarch." ]]; then
+    mv $pkg ${pkg%%.noarch.rpm}.el${OS_VERSION}.noarch.rpm
+  fi
+done
+cp -R dist/*.rpm ${DEST_DIR}/rpms
+
+popd
