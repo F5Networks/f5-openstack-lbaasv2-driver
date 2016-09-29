a32acdda3d582a92c4cf06d40a675f3f6e2269c7
Rich Browne <r.browne@f5.com>
2016-09-29 16:30:02 -0600

    Hand merging changes to setup.py file

--- setup.py
+++ setup.py
@@ -41,11 +41,6 @@ setup(name='f5-openstack-lbaasv2-driver',
       install_requires=[],
 
       packages=find_packages(),
-      entry_points={
-          'tempest.test_plugins': [
-              'f5-lbaasv2-driver-tempest-plugin = f5lbaasdriver.test.tempest.plugin:F5LBaaSv2DriverTempestPlugin'
-          ],
-      },
       classifiers=['Development Status :: 5 - Production/Stable',
                    'License :: OSI Approved :: Apache Software License',
                    'Operating System :: OS Independent',
