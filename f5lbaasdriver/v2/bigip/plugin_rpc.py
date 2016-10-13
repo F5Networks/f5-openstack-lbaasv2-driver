16d6e8b3e8e16c4787c73963f881a88d8b51c7bf
Rich Browne <r.browne@f5.com>
2016-10-13 12:12:33 -0600

    Adding l7 rule status updates/delete callbacks

--- f5lbaasdriver/v2/bigip/plugin_rpc.py
+++ f5lbaasdriver/v2/bigip/plugin_rpc.py
@@ -417,6 +417,39 @@ class LBaaSv2PluginCallbacksRPC(object):
         """Agent confirmation hook that l7 policy has been destroyed."""
         self.driver.plugin.db.delete_l7policy(context, l7policy_id)
 
+    @log_helpers.log_method_call
+    def update_l7rule_status(
+            self,
+            context,
+            l7rule_id,
+            l7policy_id,
+            provisioning_status=plugin_constants.ERROR):
+        """Agent confirmation hook to update l7 policy status."""
+        with context.session.begin(subtransactions=True):
+            try:
+                l7rule_db = self.driver.plugin.db.get_l7policy_rule(
+                    context,
+                    l7rule_id,
+                    l7policy_id
+                )
+                if (l7rule_db.provisioning_status ==
+                        plugin_constants.PENDING_DELETE):
+                    provisioning_status = plugin_constants.PENDING_DELETE
+                self.driver.plugin.db.update_status(
+                    context,
+                    models.L7Rule,
+                    l7rule_id,
+                    provisioning_status
+                )
+            except Exception as e:
+                LOG.error('Exception: update_l7policy_status: %s',
+                          e.message)
+
+    @log_helpers.log_method_call
+    def l7rule_destroyed(self, context, l7rule_id):
+        """Agent confirmation hook that l7 policy has been destroyed."""
+        self.driver.plugin.db.delete_l7policy_rule(context, l7rule_id)
+
     # Neutron core plugin core object management
 
     @log_helpers.log_method_call
