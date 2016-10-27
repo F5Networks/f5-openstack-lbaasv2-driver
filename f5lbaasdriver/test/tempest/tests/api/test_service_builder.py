5021ac2bdd324ea2d6166c31c422ec9adfe9f333
Jeffrey Longstaff <j.longstaff@f5.com>
2016-10-27 14:05:32 -0600

    Add missing provisioning_status to listeners, pools, l7

--- f5lbaasdriver/test/tempest/tests/api/test_service_builder.py
+++ f5lbaasdriver/test/tempest/tests/api/test_service_builder.py
@@ -11,8 +11,6 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-
-from pprint import PrettyPrinter
 from tempest import config
 from tempest.lib.common.utils import data_utils
 from tempest import test
@@ -32,11 +30,14 @@ class ServiceBuilderTestJSON(base.BaseTestCase):
         if not test.is_extension_enabled('lbaasv2', 'network'):
             msg = "lbaas extension not enabled."
             raise cls.skipException(msg)
+        cls.subnet = cls.get_tenant_network()
+
         cls.network_name = data_utils.rand_name('network')
         cls.network = cls.create_network(cls.network_name)
         cls.subnet = cls.create_subnet(cls.network)
         cls.create_lb_kwargs = {'tenant_id': cls.subnet['tenant_id'],
                                 'vip_subnet_id': cls.subnet['id']}
+
         cls.load_balancer = \
             cls._create_active_load_balancer(**cls.create_lb_kwargs)
         cls.load_balancer_id = cls.load_balancer['id']
@@ -58,23 +59,18 @@ class ServiceBuilderTestJSON(base.BaseTestCase):
             cls._create_pool(**cls.create_pool_kwargs))
         cls.pool_id = cls.pool['id']
 
-        # create second pool with no listener (detached)
-        cls.create_detached_pool_kwargs = {
-            'loadbalancer_id': cls.load_balancer_id,
-            'protocol': 'HTTPS',
-            'description': 'DETACHED pool',
-            'lb_algorithm': 'ROUND_ROBIN'}
-
-        cls.detached_pool = (
-            cls._create_pool(**cls.create_detached_pool_kwargs))
-        cls.detached_pool_id = cls.detached_pool['id']
-
         # create member
         cls.create_member_kwargs = {'address': '10.2.2.3',
                                     'protocol_port': 8080,
                                     'subnet_id': cls.subnet['id']}
         cls.member = (
             cls._create_member(cls.pool_id, **cls.create_member_kwargs))
+
+        cls.create_monitor_kwargs = {'pool_id': cls.pool_id,
+                                     'type': 'HTTP',
+                                     'delay': 1,
+                                     'timeout': 1,
+                                     'max_retries': 5}
 
         # get an RPC client for calling into driver
         cls.client = cls.plugin_rpc.get_client()
@@ -88,8 +84,6 @@ class ServiceBuilderTestJSON(base.BaseTestCase):
         # get service object from driver
         res = self.client.call(self.context, "get_service_by_loadbalancer_id",
                                loadbalancer_id=self.load_balancer_id)
-        pp = PrettyPrinter(indent=2)
-        pp.pprint(res)
 
         # loadbalancer
         self.assertEqual(self.load_balancer_id,
@@ -104,17 +98,12 @@ class ServiceBuilderTestJSON(base.BaseTestCase):
                                 self.create_listener_kwargs)
 
         # check attached pool
-        self.assertEqual(2,
+        self.assertEqual(1,
                          len(res['pools']),
-                         message="Expected 2 pools.")
+                         message="Expected 1 pool.")
         self.check_lbaas_object(
             self.find_object(self.pool_id, res['pools']),
             self.create_pool_kwargs)
-
-        # check detached pool
-        self.check_lbaas_object(
-            self.find_object(self.detached_pool_id, res['pools']),
-            self.create_detached_pool_kwargs)
 
         # members
         self.assertEqual(1,
@@ -150,4 +139,6 @@ class ServiceBuilderTestJSON(base.BaseTestCase):
 
     @test.attr(type='smoke')
     def test_service_builder(self):
+
+        self._create_health_monitor(**self.create_monitor_kwargs)
         self.check_service_object()
