e2b57a3a186b67cc356b90a5ade7992b1b5fcbcd
Paul Breaux <p.breaux@f5.com>
2016-11-17 11:07:25 -0700

    Adding new scenario tests with ammended teardown, so that members are removed before the pool itself is removed.

--- f5lbaasdriver/test/tempest/tests/scenario/test_l7policy.py
+++ f5lbaasdriver/test/tempest/tests/scenario/test_l7policy.py
@@ -15,11 +15,26 @@
 from f5lbaasdriver.test.tempest.tests.scenario import f5_base
 
 import pytest
-import urllib2
+import requests
 
 
 class TestL7PolicyBasic(f5_base.F5BaseTestCase):
     '''Testing L7Policies deployed to BIG-IP device.'''
+
+    def remove_members(self):
+        # Due to issue 393 in github, nodes from the device are not deleted
+        # unless the members are explicitly deleted
+        self.members_client.delete_member(
+            self.pool.get('id'),
+            self.members['primary'].get('id')
+        )
+        self._wait_for_load_balancer_status(self.load_balancer.get('id'))
+        self.members_client.delete_member(
+            self.detached_pool.get('id'),
+            self.members['secondary'].get('id')
+        )
+        self._wait_for_load_balancer_status(self.load_balancer.get('id'))
+        super(TestL7PolicyBasic, self).tearDown()
 
     def _redirect_pool_args(self):
         self.redirect_pool_args = {
@@ -32,27 +47,31 @@ class TestL7PolicyBasic(f5_base.F5BaseTestCase):
             'listener_id': self.listener.get('id'),
             'admin_state_up': 'true', 'action': 'REJECT'}
 
+    def _redirect_url_args(self):
+        self.redirect_url_args = {
+            'listener_id': self.listener.get('id'),
+            'admin_state_up': 'true', 'action': 'REDIRECT_TO_URL',
+            'redirect_url': 'http://10.190.0.20'}
+
     def setUp(self):
         super(TestL7PolicyBasic, self).setUp()
         self._redirect_pool_args()
         self._reject_pool_args()
+        self._redirect_url_args()
 
-    def _run_traffic(self, expected_server, headers=None):
+    def _run_traffic(
+            self, expected_server, headers=None, cookies=None,
+            expected_status=200):
         print('##################')
         print('##################')
         print('making request to {}'.format(self.vip_ip))
-        import time
-        time.sleep(5)
-        contents = None
-        if headers:
-            request = urllib2.Request(
-                "http://{}".format(self.vip_ip), headers=headers)
-            contents = urllib2.urlopen(request).read()
-        else:
-            contents = urllib2.urlopen("http://{}".format(self.vip_ip)).read()
+        res = None
+        res = requests.get('http://{}'.format(self.vip_ip),
+                           headers=headers, cookies=cookies)
         print('Expected_server: {}'.format(expected_server))
-        print('Return server: {}'.format(contents))
-        assert expected_server in contents
+        print('Return server: {}'.format(res.text))
+        assert expected_server in res.text
+        assert res.status_code == expected_status
 
     def test_policy_deployment(self):
         '''Policy without rule will have no effect.'''
@@ -60,6 +79,9 @@ class TestL7PolicyBasic(f5_base.F5BaseTestCase):
         self._run_traffic('server1')
         self.l7policy = self._create_l7policy(**self.redirect_pool_args)
         self._run_traffic('server1')
+        self._delete_l7policy(self.l7policy.get('id'))
+        self._wait_for_load_balancer_status(self.load_balancer.get('id'))
+        self.remove_members()
 
     def test_policy_redirect_pool_header_equal_to(self):
         '''Redirect to pool when header seen with value equal to.'''
@@ -70,16 +92,24 @@ class TestL7PolicyBasic(f5_base.F5BaseTestCase):
         self._create_l7rule(self.l7policy.get('id'), **rule_args)
         self._run_traffic('server1')
         self._run_traffic('server2', headers={'X-HEADER': 'yes'})
+        self._delete_l7policy(self.l7policy.get('id'))
+        self._wait_for_load_balancer_status(self.load_balancer.get('id'))
+        self._run_traffic('server1', headers={'X-HEADER': 'yes'})
+        self.remove_members()
 
     def test_policy_redirect_pool_cookie_contains(self):
         '''Redirect to pool when cookie contains value.'''
 
-        self.l7policy = self._create_l7policy(**self._redirect_pool_args)
+        self.l7policy = self._create_l7policy(**self.redirect_pool_args)
         rule_args = {'type': 'COOKIE', 'compare_type': 'CONTAINS',
-                     'key': 'TEST_COOKIE', 'value': 'cookie_value'}
+                     'key': 'cook', 'value': 'es'}
         self._create_l7rule(self.l7policy.get('id'), **rule_args)
         self._run_traffic('server1')
-        self._run_traffic('server2', headers={'Cookie': 'TEST_COOKIE=value'})
+        self._run_traffic('server2', cookies={'cook': 'test'})
+        self._delete_l7policy(self.l7policy.get('id'))
+        self._wait_for_load_balancer_status(self.load_balancer.get('id'))
+        self._run_traffic('server1', cookies={'cook': 'test'})
+        self.remove_members()
 
     def test_policy_reject_header_starts_with(self):
         '''Reject traffic when header value starts with value.'''
@@ -92,15 +122,40 @@ class TestL7PolicyBasic(f5_base.F5BaseTestCase):
         self._run_traffic('server1', headers={'X-HEADER': 'test'})
         with pytest.raises(Exception) as ex:
             self._run_traffic('server2', headers={'X-HEADER': 'real_head'})
-        import time
-        print('###########')
-        print(type(ex))
-        print(ex)
-        time.sleep(5)
-        assert 'test' in ex.value.message
+        assert 'Connection aborted' in str(ex)
+        self._delete_l7policy(self.l7policy.get('id'))
+        self._wait_for_load_balancer_status(self.load_balancer.get('id'))
+        self._run_traffic('server1', headers={'X-HEADER': 'test'})
+        self.remove_members()
+
+    def test_policy_reject_header_starts_with_issue_393(self):
+        '''Test should fail until pool delete also deletes nodes on device.'''
+
+        self.l7policy = self._create_l7policy(**self.reject_pool_args)
+        rule_args = {'type': 'HEADER', 'compare_type': 'CONTAINS',
+                     'key': 'X-HEADER', 'value': 'real'}
+        self._create_l7rule(self.l7policy.get('id'), **rule_args)
+        self._run_traffic('server1')
+        self._run_traffic('server1', headers={'X-HEADER': 'test'})
+        with pytest.raises(Exception) as ex:
+            self._run_traffic('server2', headers={'X-HEADER': 'real_head'})
+        assert 'Connection aborted' in str(ex)
+        self._delete_l7policy(self.l7policy.get('id'))
+        self._wait_for_load_balancer_status(self.load_balancer.get('id'))
+        self._run_traffic('server1', headers={'X-HEADER': 'test'})
 
     def test_policy_redirect_url_hostname_ends_with(self):
         '''Redirect traffic to url when hostname ends with value.'''
 
         self.l7policy = self._create_l7policy(**self.redirect_url_args)
-        #rule_args = {'type': 'REDIRECT_TO_URL'
+        rule_args = {'type': 'HOST_NAME', 'compare_type': 'ENDS_WITH',
+                     'value': '1.1.1'}
+        self._create_l7rule(self.l7policy.get('id'), **rule_args)
+        self._run_traffic('server1')
+        self._run_traffic('coverity-doc', headers={'Host': '11.1.1.1'})
+        self._run_traffic('coverity-doc', headers={'Host': '1.1.1.1'})
+        self._run_traffic('server1', headers={'Host': '10.1.32.2'})
+        self._delete_l7policy(self.l7policy.get('id'))
+        self._wait_for_load_balancer_status(self.load_balancer.get('id'))
+        self._run_traffic('server1', headers={'Host': '1.1.1.1'})
+        self.remove_members()
