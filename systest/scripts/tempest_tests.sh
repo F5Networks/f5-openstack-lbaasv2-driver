f2ce67af4896e791133fd7e2949ba5ab7180f636
Shawn Wormke <shawn@f5.com>
2017-01-04 11:29:36 -0700

    Integrate neutron-lbaas v2 tempest test into F5 internal regression system #355
    Issues:
    Fixes #355
    
    Problem:
    After final testing the results files did not appear in the reporting system correctly becaue the directories were not formatted properly.
    
    Analysis:
    * Make sure that the results are shown in trtl as a project called f5-openstack-lbaasv2-driver_liberty
    * All branch results are bbot jobs so we want to organize them by project
    * Ensure session names are unique with timestamp

--- systest/scripts/tempest_tests.sh
+++ systest/scripts/tempest_tests.sh
@@ -24,11 +24,11 @@ cd ${NEUTRON_LBAAS_DIR}
 # LBaaSv2 API test cases with F5 tox.ini file
 tox -e apiv2 -c f5.tox.ini -- \
   -lvv --tb=line \
-  --autolog-outputdir ${API_RESULTS_DIR} \
+  --autolog-outputdir ${RESULTS_DIR} \
   --autolog-session ${API_SESSION}
 
 # LBaaSv2 Scenario test cases with F5 tox.ini file
 tox -e scenariov2 -c f5.tox.ini -- \
   -lvv --tb=line \
-  --autolog-outputdir ${SCENARIO_RESULTS_DIR} \
+  --autolog-outputdir ${RESULTS_DIR} \
   --autolog-session ${SCENARIO_SESSION}
