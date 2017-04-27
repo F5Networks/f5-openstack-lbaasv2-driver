7cfa7b6fdbfd64effc4b408dc8683b4ca0ee3ed7
Sanaz <s.zakeri@f5.com>
2017-04-27 17:37:27 -0600

    Update run_neutron_lbaas.sh

--- systest/scripts/run_neutron_lbaas.sh
+++ systest/scripts/run_neutron_lbaas.sh
@@ -41,6 +41,5 @@ tox -e scenariov2 -c f5.tox.ini --sitepackages -- \
   --autolog-outputdir ${RESULTS_DIR} \
   --autolog-session ${SCENARIO_SESSION}
 
-
 # Returning pass so that all tests run
 exit 0
