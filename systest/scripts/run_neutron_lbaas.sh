6f704d8219cd34cd5a661dd64324be30b1f0012f
Za Wilgustus <za@f5.com>
2017-07-14 13:58:38 -0600

    add sudo -E to tox invocations

--- systest/scripts/run_neutron_lbaas.sh
+++ systest/scripts/run_neutron_lbaas.sh
@@ -20,8 +20,8 @@ set -x
 # Create .pytest.rootdir files at the root of the driver and neutron-lbaas
 # respositories to make the results suite names be rooted at the top-level
 # of the respective test repository
-touch ${MAKEFILE_DIR}/../f5lbaasdriver/test/tempest/tests/.pytest.rootdir
-touch ${NEUTRON_LBAAS_DIR}/neutron_lbaas/tests/tempest/v2/.pytest.rootdir
+sudo -E touch ${PROJROOTDIR}f5lbaasdriver/test/tempest/tests/.pytest.rootdir
+sudo -E touch ${NEUTRON_LBAAS_DIR}/neutron_lbaas/tests/tempest/v2/.pytest.rootdir
 
 
 # The following tox commands will fail, if the ${EXCLUDE_FILE}
@@ -30,26 +30,26 @@ touch ${NEUTRON_LBAAS_DIR}/neutron_lbaas/tests/tempest/v2/.pytest.rootdir
 # Navigate to the root of the repo, where the tox.ini file is found
 cd ${MAKEFILE_DIR}/../
 
-tox --sitepackages -e tempest -c tox.ini -- \
+sudo -E bash -c "tox --sitepackages -e tempest -c tox.ini -- \
   --meta ${EXCLUDE_DIR}/${EXCLUDE_FILE} \
   -lravv \
   --autolog-outputdir ${RESULTS_DIR} \
-  --autolog-session ${DRIVER_TEMPEST_SESSION}
+  --autolog-session ${DRIVER_TEMPEST_SESSION}"
 
 cd ${NEUTRON_LBAAS_DIR}
 
 # LBaaSv2 API test cases with F5 tox.ini file
-tox -e apiv2 -c f5.tox.ini --sitepackages -- \
+sudo -E bash -c "tox -e apiv2 -c f5.tox.ini --sitepackages -- \
   --meta ${EXCLUDE_DIR}/${EXCLUDE_FILE} \
   -lvv \
   --autolog-outputdir ${RESULTS_DIR} \
-  --autolog-session ${API_SESSION}
+  --autolog-session ${API_SESSION}"
 
 # LBaaSv2 Scenario test cases with F5 tox.ini file
-tox -e scenariov2 -c f5.tox.ini --sitepackages -- \
+sudo -E bash -c "tox -e scenariov2 -c f5.tox.ini --sitepackages -- \
   --meta ${EXCLUDE_DIR}/${EXCLUDE_FILE} \
   -lvv \
   --autolog-outputdir ${RESULTS_DIR} \
-  --autolog-session ${SCENARIO_SESSION}
+  --autolog-session ${SCENARIO_SESSION}"
 
 exit 0
