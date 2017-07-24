5214cbdb66442321269bda1f137c1c713ea7faaf
John Gruber <john.t.gruber@gmail.com>
2017-07-24 06:08:24 -0500

    added unit test for rebind_loadbalancers()

--- f5lbaasdriver/v2/bigip/test/test_agent_scheduler.py
+++ f5lbaasdriver/v2/bigip/test/test_agent_scheduler.py
@@ -55,6 +55,28 @@ def test_schedule():
     assert agent
 
 
+def test_rebind_loadbalancers():
+
+    plugin = mock.MagicMock()
+    context = mock.MagicMock()
+    sched = agent_scheduler.TenantScheduler()
+    sched.get_agents_in_env = mock.MagicMock(name='get_agents_in_env')
+    agents_in_env = [{'fake_agent': {
+            'id': 'test_agent_2_id',
+            'alive': True,
+            'admin_state_up': True,
+            'configurations': {
+                'environment_prefix': 'prod',
+                'environment_group_number': 2
+            }
+        }}]
+    sched.get_agents_in_env.return_value = agents_in_env
+    context.session.query.all = mock.MagicMock(
+        name='all', return_value=[type('test', (), {})()])
+    context.session.add = mock.MagicMock(name='add', return_value=None)
+    sched.rebind_loadbalancers(context, plugin, 'prod', 2, agents_in_env[0])
+
+
 def test_get_lbaas_agent_hosting_loadbalancer_none():
     mock_plugin = mock.MagicMock(name='plugin')
     mock_plugin.db.get_agent_hosting_loadbalancer.return_value = None
@@ -181,6 +203,8 @@ def test_get_lbaas_agent_hosting_loadbalancer_agent_dead_env_agents_active():
             }
         }}]
     sched.get_agents_in_env.return_value = agents_in_env
+    sched.rebind_loadbalancers = mock.MagicMock(name='rebind_loadbalancers')
+    sched.rebind_loadbalancers.return_value = agents_in_env[0]
     res = sched.get_lbaas_agent_hosting_loadbalancer(
         mock_plugin, mock_cxt, 'test_lb_id', env='test_env')
     assert res == {'agent': agents_in_env[0]}
