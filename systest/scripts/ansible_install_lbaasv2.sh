7b15f8cc8da5206a41a9192bbc83f390d4cacb7a
John Gruber <john.t.gruber@gmail.com>
2017-08-16 13:34:56 -0500

    adding systest for F5Network Mitaka merge

--- systest/scripts/ansible_install_lbaasv2.sh
+++ systest/scripts/ansible_install_lbaasv2.sh
@@ -8,9 +8,8 @@ OS_CONTROLLER_IP=`/tools/bin/tlc --sid ${TEST_SESSION} symbols \
 SSH_CMD="ssh -i /home/jenkins/.ssh/id_rsa -o StrictHostKeyChecking=no testlab@${OS_CONTROLLER_IP}"
 BIGIP_IP=`${SSH_CMD} "cat /home/testlab/ve_mgmt_ip"`
 BIGIP_IP=${BIGIP_IP%%[[:cntrl:]]}
-NETWORK_TYPES=${TEST_TENANT_NETWORK_TYPE}
 AGENT_LOC=git+https://github.com/F5Networks/f5-openstack-agent.git@${BRANCH}
-DRIVER_LOC=git+https://github.com/F5Networks/f5-openstack-lbaasv2-driver.git@${BRANCH}
+DRIVER_LOC=${DRIVER_PIP_INSTALL_LOCATION}
 NEUTRON_DRIVER_LOC=https://raw.githubusercontent.com/F5Networks/neutron-lbaas/stable/${BRANCH}/neutron_lbaas/drivers/f5/driver_v2.py
 
 # Since we don't do anything special in the __init__.py file, we can pull it from anywhere for now
@@ -21,10 +20,10 @@ EXTRA_VARS="agent_pkg_location=${AGENT_LOC} driver_pkg_location=${DRIVER_LOC} ne
 
 if [[ $TEST_OPENSTACK_CLOUD == 'undercloud' ]]; then
     GLOBAL_ROUTED_MODE="False"
-    if [[ $NETWORK_TYPES == 'vlan' ]]; then
+    if [[ $TEST_TENANT_NETWORK_TYPE == 'vlan' ]]; then
         ADVERTISED_TUNNEL_TYPES=""
     else
-        ADVERTISED_TUNNEL_TYPES=${NETWORK_TYPES}
+        ADVERTISED_TUNNEL_TYPES=${TEST_TENANT_NETWORK_TYPE}
     fi
     EXTRA_VARS="${EXTRA_VARS} advertised_tunnel_types=${ADVERTISED_TUNNEL_TYPES}"
 else
@@ -34,9 +33,9 @@ fi
 EXTRA_VARS="${EXTRA_VARS} neutron_lbaas_init_location=${NEUTRON_INIT_LOC} restart_all_neutron_services=true remote_user=testlab"
 EXTRA_VARS="${EXTRA_VARS} f5_global_routed_mode=${GLOBAL_ROUTED_MODE} bigip_netloc=${BIGIP_IP} agent_service_name=f5-openstack-agent.service"
 EXTRA_VARS="${EXTRA_VARS} use_barbican_cert_manager=True neutron_lbaas_shim_install_dest=/usr/lib/python2.7/site-packages/neutron_lbaas/drivers/f5"
-
-sudo -E bash -c "echo [hosts] > /home/jenkins/zancas_tmp/ansible_conf.ini"
-sudo -E bash -c "echo \"${OS_CONTROLLER_IP} ansible_ssh_common_args='-o StrictHostKeyChecking=no' host_key_checking=False ansible_connection=ssh ansible_ssh_user=testlab ansible_ssh_private_key_file=/root/id_rsa\" >> /home/jenkins/zancas_tmp/ansible_conf.ini"
+sudo -E chown -Rf jenkins:jenkins /home/jenkins/container_mailbox
+bash -c "echo [hosts] > /home/jenkins/container_mailbox/ansible_conf.ini"
+bash -c "echo \"${OS_CONTROLLER_IP} ansible_ssh_common_args='-o StrictHostKeyChecking=no' host_key_checking=False ansible_connection=ssh ansible_ssh_user=testlab ansible_ssh_private_key_file=/root/id_rsa\" >> /home/jenkins/container_mailbox/ansible_conf.ini"
 
 sudo -E docker pull docker-registry.pdbld.f5net.com/f5-openstack/ansiblemicroservice
 sudo -E docker run \
