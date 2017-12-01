072f8b2b0a473a5414287ea2c96151b95e2511bb
Kevin Larkin <k.larkin@f5.com>
2017-11-30 21:01:47 -0700

    Move systest artifacts to internal/private repo.
    - We want to remove systest artifacts from our product repos and
      move them to a dedicated internal repo (openstack-systest-common).

--- systest/scripts/record_results.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-# - copy results files to nfs (note that the nfs results directory is mounted
-#    inside the CI worker's home directory)
-cp -r $WORKSPACE/systest/test_results/* $CI_RESULTS_DIR/
