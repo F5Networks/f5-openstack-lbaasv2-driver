7d18e31059209229f2b120191df0fbfd43581593
Kevin Larkin <k.larkin@f5.com>
2017-12-08 15:17:24 -0700

    Remove leftover references to systest artifacts.
    - When we recently removed systest artifacts from our product repos,
      we missed a few.  This commit removes those leftovers.

--- setup.py
+++ setup.py
@@ -28,10 +28,6 @@ setup(name='f5-openstack-lbaasv2-driver',
       long_description=readme(),
       version=f5lbaasdriver.__version__,
       entry_points={
-          'tempest.test_plugins': [
-              'f5-lbaasv2-driver-tempest-plugin = '
-              'f5lbaasdriver.test.tempest.plugin:F5LBaaSv2DriverTempestPlugin'
-          ],
           'console_scripts': ['add_f5agent_environment='
                               'f5lbaasdriver.utils.add_environment:main']},
       author='f5-openstack-lbaasv2-driver',
