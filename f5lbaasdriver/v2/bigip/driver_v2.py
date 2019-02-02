068c47e679ace0732f103a598ec1b60454e36f39
peter zhang <peter.zhang@f5.com>
2019-02-02 00:53:12 -0800

    F5 pike/stable driver code change for Openstack Queens lbaasv2
    (cherry picked from commit a729bf4c0ee4c741edff6d0a17438b334e6a2499)

--- f5lbaasdriver/v2/bigip/driver_v2.py
+++ f5lbaasdriver/v2/bigip/driver_v2.py
@@ -22,11 +22,11 @@ from oslo_log import helpers as log_helpers
 from oslo_log import log as logging
 from oslo_utils import importutils
 
-from neutron.callbacks import events
-from neutron.callbacks import registry
-from neutron.callbacks import resources
 from neutron.plugins.common import constants as plugin_constants
 from neutron_lib.api.definitions import portbindings
+from neutron_lib.callbacks import events
+from neutron_lib.callbacks import registry
+from neutron_lib.callbacks import resources
 from neutron_lib import constants as q_const
 
 from neutron_lbaas.db.loadbalancer import models
